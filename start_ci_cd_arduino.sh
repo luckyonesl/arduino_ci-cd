a1847a756b7e6b5f5edf0eac991be4f8511feae4
SL <lackner@gmx.de>
2020-11-21 19:14:45 +0100

    fix repo

--- start_ci_cd_arduino.sh
+++ start_ci_cd_arduino.sh
@@ -16,7 +16,8 @@ if [ `docker images cicd_arduinocli:latest|wc -l` -lt 2 ];then
 fi
 #the used docker image ( will not pulled )
 export arduinocli_image=cicd_arduinocli:latest	
-export DOCKERCLOUDCONNECTION='unix:///Users/stefan/Library/Containers/com.docker.docker/Data/docker.raw.sock'
+#maybe value must be quoted
+export DOCKERCLOUDCONNECTION=unix://${WKDIR}/Library/Containers/com.docker.docker/Data/docker.raw.sock
 
 export JENKINS_HOME=./JENKINSHOME
 export CASC_JENKINS_CONFIG=${WKDIR}/docker/jenkins/JCASC/
@@ -34,8 +35,7 @@ if [ ! -d ${JENKINS_HOME} ];then
 	java -jar /tmp/jenkins-plugin-manager-2.1.3.jar --verbose --skip-failed-plugins --plugin-download-directory ${JENKINS_HOME}/plugins/ --plugin-file docker/jenkins/plugins.txt --war /tmp/jenkins.war
 fi
 
-export SHAREDLIBREP=/Volumes/FRITZ.NAS/storage1/Software/jenkins_shared_lib.git
+export SHAREDLIBREP=https://github.com/luckyonesl/Jenkins_sharedlib.git
 echo "SHAREDLIBREP ${SHAREDLIBREP}"
-#export ARDOINOREP=/Volumes/FRITZ.NAS/storage1/Software/Ardoino/ardoino_projects
-export ARDOINOREP='gitolite3@192.168.2.101:ardoino_projects' 
+export ARDOINOREP='https://github.com/luckyonesl/arduino_projects.git' 
 java -Djenkins.install.runSetupWizard=false -jar /tmp/jenkins.war --enable-future-java --httpPort=8081
